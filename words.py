8ac00fca1f287c6963310c659d501c665d4e7085
venkatesh kareti <kareti.venkatesh9@gmail.com>
2017-12-25 17:06:24 +0530

    added persistent data and progress indicator features

--- words.py
+++ words.py
@@ -1,12 +1,14 @@
 import re
+import os
 import shutil
+import time
 
 # fileName = "/run/media/prime/SAMSUNG/wiki_dump/wiki.xml";
-fileName = "tempText.txt"
-wordsHead = None;
-openWordsFile = None;
-lineCountLimit = 1000;
-fileEnding = "***---***";
+fileName = "tempText.txt"       # Input text document.
+wordsHead = None;               # first node in words list
+openWordsFile = None;           # opened input file.
+lineCountLimit = 5;
+fileEnding = "***---***";       # file end tag; has size as it's count.
 
 class wordsTree():
 
@@ -20,7 +22,8 @@ class wordsTree():
         for i in "abcdefghijklmnopqrstuvwxyz'":
             self.words[i] = None;
             self.ends[i] = 0;
-        
+            
+
         if(index+1 < length):
             self.words[word[index]] = wordsTree(word,index+1,length = length);
         elif(index+1 == length):
@@ -53,7 +56,7 @@ class wordsTree():
 
     
 
-    # search a word and return's the end wordTree node.
+    # search a word and return's the end wordTree node; returns None if not found.
     def searchWord(self,word = "",index = 0):
         ret_val = None;
         
@@ -62,10 +65,10 @@ class wordsTree():
             return None;
         
         if(self.ends[word[index]] > 0 and
-             index == len(word)-1):
+             index == len(word)-1): # word found
             ret_val = self;  
         elif(self.words[word[index]] != None and
-             index != len(word)-1):
+             index != len(word)-1): 
             ret_val =  self.words[word[index]].searchWord(word = word, index = index + 1);
         else:
             ret_val = None;
@@ -97,9 +100,8 @@ class wordsTree():
     def writeWordsFile(self,semiWord = "",openWordsFile = None):
         # param check
         if(openWordsFile == None):
-                print("No open words write file passed.");
-                return None;
-        
+            raise Exception("No open words write file passed to writes wordsTree");
+
         
         for i in self.words:
             if(self.words[i] != None):
@@ -133,6 +135,7 @@ class wordsTree():
             wordEndNode = self.searchWord(word);
             # TODO just added word!how does wordEndNode=None at any point? check!
             if(wordEndNode == None):
+                print("Unexpected-Error: "+word);
                 continue;
             else:
                 wordEndNode.ends[word[-1]] = int(count);
@@ -142,38 +145,137 @@ class wordsTree():
 # -----------------------------------------------------------------------------------------
 
 
-def updateWordsListFile(file = "",position = 0):
+# display progress bar
+def displayProgress(continueFrom = 0, currentProgress = 0, outOfTotal = 100):
+    # calculate no.of progress values
+    continueFrom = int((continueFrom*100)/outOfTotal);
+    currentProgress = int((currentProgress*100)/outOfTotal);
+    outOfTotal = 100;
+    
+    currentSymbol = '+';
+    count = 0;
+    
+    print("\rProgress: [",end = "");
+    while(count < outOfTotal):
+        # print progress
+        if(count == currentProgress and currentSymbol == '='):
+            print(">", end = "");
+        else:
+            print(currentSymbol, end = "");
+        count += 1;
+
+        # check symbol change
+        if(currentSymbol != '=' and
+           count > continueFrom and count <= currentProgress):
+            currentSymbol = '=';
+        elif(count > currentProgress):
+            currentSymbol = ' ';
+           
+    print("] "+str(currentProgress)+"%",end = "");
+
+    return True;
+
+
+
+    
+# loads persistent data from file with corruption protection.
+def loadPersistentData(fileName = ""):
+    # param check
+    if(fileName == None or fileName == ""):
+        raise Exception("file name error while loading Persistent data.");
+
+    
+    wordsListFile = fileName+".words";
+    seekPositionFile = fileName + ".seek";
+    backUpExt = ".back";
+
+        
+    # init wordstree
+    wordsHead = wordsTree();
+    if(wordsHead == None):
+        raise Exception("Error: failed to make wordsHead");
+    
+    # init seekPosition
+    seekPosition = 0;
+
+    # choose un-corrupted persistent file
+    if(os.path.isfile(wordsListFile) and os.path.isfile(wordsListFile+backUpExt)):  
+        if(os.path.getsize(wordsListFile) > os.path.getsize(wordsListFile+backUpExt)):
+            wordsListFile = fileName+".words";
+            seekPositionFile = fileName + ".seek";
+        else:
+            backUpExt = ".back";
+            wordsListFile = fileName+".words" + backUpExt;
+            seekPositionFile = fileName + ".seek" + backUpExt;
+    else:
+        return [seekPosition, wordsHead];
+
+    
+    # update words list
+    try:
+        openWordsFile = open(wordsListFile,"r");
+        wordsHead.readWordsFile(openWordsFile = openWordsFile);
+        openWordsFile.close();
+    except Exception as e:
+        print("FIRST Time running; No stored words file  "+str(e));
+
+
+    # get seek position
+    try:
+        openSeekFile = open(seekPositionFile,"r");
+        seekPosition = int(openSeekFile.read());
+        openSeekFile.close();
+    except Exception as e:
+        print("First time reading File: "+file);
+
+    return [seekPosition,wordsHead];
+
+
+
+
+
+def updatePersistentFile(file = "",position = 0):
 
     # param check
     if(file == None or file == ""):
         return False;
 
-    
+    # prepare file names
     wordsListFile = file+".words";
+    seekPositionFile = file + ".seek";
+    backUpExt = ".back";
 
     
     # back-up existing words file
     try:
-        shutil.copyfile(src = wordsListFile, dst = wordsListFile+".back");
+        shutil.copyfile(src = wordsListFile, dst = wordsListFile+backUpExt);
     except FileNotFoundError as e:
-        print("",end = "");
+        print("",end = "");     # because - First time backup. ignore
     except Exception as e:
         raise Exception("ERROR: while making a back-up of words file!,,,"+str(e));
-    
 
-    # update words list #TODO take a backup of wordsListFile.
+    # back-up existing seek file
+    try:
+        shutil.copyfile(src = seekPositionFile, dst = seekPositionFile+backUpExt);
+    except FileNotFoundError as e:
+        print("",end = "");     # because - First time backup. ignore
+    except Exception as e:
+        raise Exception("ERROR: while making a back-up of seek file!,,,"+str(e));
+
+
+    
+    # update words list
     try:
         openFile = open(wordsListFile,"w");
         wordsHead.writeWordsFile(semiWord = "",openWordsFile = openFile);
-        openFile.write(fileEnding + str(position) + "\n");
-        openFile.close();           
+        openFile.close();
     except Exception as e:
         raise Exception("ERROR: While writing current words list to file!,,,"+str(e));
 
     
     # update file position
     try:
-        openFile = open(file+".seek","w");
+        openFile = open(seekPositionFile,"w");
         if(openFile == None):
             return False;
         openFile.write(str(position));
@@ -181,53 +283,37 @@ def updateWordsListFile(file = "",position = 0):
     except Exception as e:
         raise Exception("ERROR: while writing seek!,,,"+str(e));
 
+    
     # return 
     return True;
 
 
 
 
-def collectwords(file = ""):
+def collectWords(file = ""):
 
     global wordsHead;
+    seekPosition = 0;
+    
     lineCount = 0;
     
     # param check
     if(file == None or file == ""): # file name check
         return None;
 
-
-    # init wordstree
-    wordsHead = wordsTree();
-    if(wordsHead == None):
-        raise Exception("Error: failed to make wordsHead");
-
     
-    # collect stored words
+    # load Persistent Data (prev. words and seek position.)
     try:
-        wordsListFile = file+".words";
-        openWordsFile = open(wordsListFile,"r");
-        wordsHead.readWordsFile(openWordsFile = openWordsFile);
-        openWordsFile.close();
+        [seekPosition,wordsHead] = loadPersistentData(fileName = file);
     except Exception as e:
-        print("FIRST Time running; No stored words file  "+str(e));
-    
-    
-    
+        print("Error: Unable to load Persistent data! Continuing;..."+str(e));
+
     # open file
     try:
         openFile = open(file,"r");
         if(openFile == None):
             return None;
-        # get seek position. and seek.
-        try:
-            seekFile = open(file+".seek","r");
-            seekPosition = int(seekFile.read());
-            openFile.seek(seekPosition);
-            seekFile.close();
-        except Exception as e:
-            print("First time reading File: "+file);
-            
+        openFile.seek(seekPosition);            
     except Exception as e:
         print(e);
         raise e;
@@ -238,9 +324,15 @@ def collectwords(file = ""):
     line = openFile.readline();
     while(line):
         if(lineCount > lineCountLimit):
+            # show progress
+            displayProgress(continueFrom = 0,
+                            currentProgress = openFile.tell(),
+                            outOfTotal = os.path.getsize(file));
+            time.sleep(1);
+            # save current progress
+            updatePersistentFile(file = file, position = openFile.tell());
             lineCount = 0;
-            updateWordsListFile(file = file, position = openFile.tell());
-        # TODO write current position to file. 
+            
         for word in re.findall(r'[a-zA-Z\']*', line):
             if(word):
                 wordsHead.append(word = word.lower(), index = 0, length = len(word));
@@ -248,7 +340,7 @@ def collectwords(file = ""):
         line = openFile.readline();
 
     # finally update the file once.
-    updateWordsListFile(file = file, position = openFile.tell());
+    updatePersistentFile(file = file, position = openFile.tell());
 
     # close the file.
     openFile.close();
@@ -257,6 +349,5 @@ def collectwords(file = ""):
 # -----------------------------------------------------------------------------------------
 
 
-collectwords(file = fileName);
-wordsHead.printWords();
-
+collectWords(file = fileName);
+# wordsHead.printWords();
